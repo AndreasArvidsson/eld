f8bd1e889b7e4414e169b56cdb60ab04457d046d
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-09-17 15:18:05 +0200

    Upper case literal kind in fixture serialization

--- src/main/java/mylang/parser/LiteralKind.java
+++ src/main/java/mylang/parser/LiteralKind.java
@@ -1,21 +1,10 @@
 package mylang.parser;
 
 public enum LiteralKind {
-    INT("int"),
-    FLOAT("float"),
-    BOOL("bool"),
-    CHAR("char"),
-    STRING("string"),
-    NULL("null");
-
-    private final String name;
-
-    private LiteralKind(final String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
+    INT,
+    FLOAT,
+    BOOL,
+    CHAR,
+    STRING,
+    NULL;
 }
